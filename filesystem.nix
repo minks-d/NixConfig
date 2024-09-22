24331511ee524f21effd59fde2f1dd0cacb69a70
Daniel <danielminks1230@gmail.com>
2024-09-22 11:40:12 -0400

    Recovery

---
+++ filesystem.nix
@@ -0,0 +1,18 @@
+{pkgs,...}:
+
+{
+
+fileSystems = {
+
+	"/" = {
+		device = "/dev/disk/by-label/NIXROOT";
+		fsType = "ext4";
+	};
+
+	"/boot" = {
+
+		device = "/dev/disk/by-label/NIXBOOT";
+		fsType = "vfat";
+	};
+};
+}
---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Flake configuration for Daniel Minks PC";
+
+  inputs = {
+    # NixOS official package source, using the nixos-24.05 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    # Please replace my-nixos with your hostname
+    nixosConfigurations.minksdHome= nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+      ];
+    };
+  };
+}
