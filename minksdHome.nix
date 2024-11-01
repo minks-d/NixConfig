cff3cb37d5d8755af2dc75f85f3d10f19b91bd58
Daniel Minks <danielminks1230@gmail.com>
2024-10-31 23:40:03 -0400

    Updated flake, changed nvidia to open driver

--- minksdHome.nix
+++ minksdHome.nix
@@ -81,7 +81,7 @@ inputs.nixpkgs.lib.nixosSystem rec {
           modesetting.enable = true;
           powerManagement.enable = true;
           powerManagement.finegrained = false;
-          open = false;
+          open = true;
           nvidiaSettings = true;
         };
         opengl.enable = true;
