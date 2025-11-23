fdd07b28df292db434842f53434df6a7e74f2241
Daniel Minks <danielminks1230@gmail.com>
2025-11-23 08:21:50 -0500

    Reapply "Elixir and Emacs + Elixir updates"
    This reverts commit 02e23dd3ebf077e8957940ba9ea09cc76f14d96c.

--- modules/common/emacs/default.nix
+++ modules/common/emacs/default.nix
@@ -17,12 +17,15 @@
       emacs.pkgs.lsp-mode
       emacs.pkgs.flycheck
       emacs.pkgs.rust-mode
+      emacs.pkgs.eglot
       
 
     ];
     home-manager.users.${config.user} = {
       home.file.".emacs.d/init.el".source = ./init.el;
       home.file.".emacs.d/rust.el".source = ./rust.el;
+      home.file.".emacs.d/elixir.el".source = ./elixir.el;
+      
     };
   };
 }
