5a128e74d288becd0d8ceae57a70d75a2b912bde
Daniel Minks <danielminks1230@gmail.com>
2025-11-28 18:15:51 -0500

    Trying tmpfs for /tmp

--- minksdHome/boot.nix
+++ minksdHome/boot.nix
@@ -1,6 +1,12 @@
 { upkgs, ... }:
 {
   boot = {
+    tmp = {
+      useTmpfs = true;
+      tmpfsSize = "10%";
+      tmpfsHugeMemoryPages = "within_size";
+      cleanOnBoot = true;
+    };
     supportedFilesystems = [ "ntfs" ];
     initrd = {
       availableKernelModules = [
