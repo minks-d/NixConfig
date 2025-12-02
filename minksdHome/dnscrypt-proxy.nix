69b56c0d243cd69c1d748f51dde6cfc89d8a028b
Daniel Minks <danielminks1230@gmail.com>
2025-12-01 19:41:09 -0500

    Fixed DNS issues, moved to entirely ODoH, and configured it correctly this time I think

--- minksdHome/dnscrypt-proxy.nix
+++ minksdHome/dnscrypt-proxy.nix
@@ -27,14 +27,38 @@ in
 
     # See https://github.com/DNSCrypt/dnscrypt-proxy/blob/master/dnscrypt-proxy/example-dnscrypt-proxy.toml
     settings = {
-      sources.public-resolvers = {
+      
+      bootstrap_resolvers = ["1.1.1.1:53" "8.8.8.8:53" "9.9.9.11:53"];
+      sources.odoh-servers = {
         urls = [
-          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
           "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/odoh-servers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/odoh-servers.md"
         ];
-        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3"; # See https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
-        cache_file = "/var/lib/${StateDirectory}/public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        cache_file = "/var/lib/${StateDirectory}/odoh-servers.md";
+        refresh_delay = 73;
+        prefix = "";
       };
+      sources.odoh-relays = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/odoh-relays.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/odoh-relays.md"
+        ];
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        cache_file = "/var/lib/${StateDirectory}/odoh-relays.md";
+        refresh_delay = 73;
+        prefix = "";
+      };
+
+      anonymized_dns = {
+        routes = [
+        {
+          server_name = "*";
+          via = ["*"];
+        }
+        ];
+      };
+        
 
       # Use servers reachable over IPv6 -- Do not enable if you don't have IPv6 connectivity
       ipv6_servers = hasIPv6Internet;
@@ -59,4 +83,32 @@ in
   };
 
   systemd.services.dnscrypt-proxy.serviceConfig.StateDirectory = StateDirectory;
+
+  networking.nftables = {
+    enable = true;
+
+    ruleset = ''
+      table inet filter {
+        chain output {
+          type filter hook output priority 0; policy accept;
+
+          # Allow localhost DNS for dnscrypt-proxy
+          ip daddr 127.0.0.1 udp dport 53 accept
+          ip6 daddr ::1 udp dport 53 accept
+          ip daddr 127.0.0.1 tcp dport 53 accept
+          ip6 daddr ::1 tcp dport 53 accept
+
+          # Allow dnscrypt-proxy to talk to upstream servers
+          # Replace <DNSCRYPT-UID> with:
+          # ps -o uid,user,pid,cmd -C dnscrypt-proxy
+          meta skuid 62582 udp dport { 443, 853 } accept
+          meta skuid 62582 tcp dport { 443, 853 } accept
+
+          # Block all other outbound DNS
+          udp dport { 53, 853 } drop
+          tcp dport { 53, 853 } drop
+        }
+      }
+    '';
+  };
 }
