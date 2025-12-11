4dc246c638c3a9bf65a9cc6467cca95fb8bd7336
Daniel Minks <danielminks1230@gmail.com>
2025-12-10 19:16:53 -0500

    Modprobe blacklisting

--- modules/nixos/security/default.nix
+++ modules/nixos/security/default.nix
@@ -2,7 +2,7 @@
   imports = [
     ./age.nix
     ./wrappers.nix
-
+    ./modprobe.nix
   ];
 
   config = {
@@ -26,11 +26,45 @@
             }
           }
         });
-      ''; 
-    };    
+      '';
+
+      #Sets the kernel's resource limit (ulimit -c 0)
+      pam.loginLimits = [
+        {
+          domain = "*"; # Applies to all users/sessions
+          type = "-"; # Set both soft and hard limits
+          item = "core"; # The soft/hard limit item
+          value = "0";   # Core dumps size is limited to 0 (effectively disabled)
+        }
+      ];
+    };
     #Currently bugged such that declarative users cant have their passwords loaded at runtime
     #services.userborn.enable = true;
+
+    #https://saylesss88.github.io/nix/hardening_NixOS.html#hardening-systemd
+    systemd.coredump.enable = false;
     
+
+    users.groups.netdev = {};
+    services = {
+      dbus.implementation = "broker";
+      logrotate.enable = true;
+      journald = {
+        upload.enable = false; # Disable remote log upload (the default)
+        extraConfig = ''
+        SystemMaxUse=500M
+        SystemMaxFileSize=50M
+      '';
+      };
+    };
+    
+    # Only needed for WWAN/3G/4G modems, otherwise it runs `mmcli` unnecessarily
+    networking.modemmanager.enable = false;
+    # Bluetooth has a long history of vulnerabilities
+    hardware.bluetooth.enable = false;
+    # Prefer manual upgrades on a hardened system
+    system.autoUpgrade.enable = false;
+
   };
 
 }
---
+++ modules/nixos/security/modprobe.nix
@@ -0,0 +1,108 @@
+{config, ...}:{
+
+  config = {
+    boot.blacklistedKernelModules = [
+      # firewire and thunderbolt
+      "firewire-core"
+      "firewire_core"
+      "firewire-ohci"
+      "firewire_ohci"
+      "firewire_sbp2"
+      "firewire-sbp2"
+      "firewire-net"
+      "thunderbolt"
+      "ohci1394"
+      "sbp2"
+      "dv1394"
+      "raw1394"
+      "video1394"
+      # Obscure networking protocols
+      "dccp"   # Datagram Congestion Control Protocol
+      "sctp"  # Stream Control Transmission Protocol
+      "rds"  # Reliable Datagram Sockets
+      "tipc"  # Transparent Inter-Process Communication
+      "n-hdlc" # High-level Data Link Control
+      "ax25"  # Amateur X.25
+      "netrom"  # NetRom
+      "x25"     # X.25
+      "rose"
+      "decnet"
+      "econet"
+      "af_802154"  # IEEE 802.15.4
+      "ipx"  # Internetwork Packet Exchange
+      "appletalk"
+      "psnap"  # SubnetworkAccess Protocol
+      "p8023"  # Novell raw IEE 802.3
+      "p8022"  # IEE 802.3
+      "can"   # Controller Area Network
+      "atm"
+      # Various rare filesystems
+      "cramfs"
+      "freevxfs"
+      "jffs2"
+      "hfs"
+      "hfsplus"
+      "udf"
+      "squashfs"  # compressed read-only file system used for Live CDs
+      "cifs"  # cmb (Common Internet File System)
+      "nfs"  # Network File System
+      "nfsv3"
+      # "nfsv4"
+      # "ksmbd"  # SMB3 Kernel Server
+      "gfs2"  # Global File System 2
+      # vivid driver is only useful for testing purposes and has been the
+      # cause of privilege escalation vulnerabilities
+      "vivid"
+      # disable GNSS
+      "gnss"
+      "gnss-mtk"
+      "gnss-serial"
+      "gnss-sirf"
+      "gnss-usb"
+      "gnss-ubx"
+
+      # https://en.wikipedia.org/wiki/Bluetooth#History_of_security_concerns
+      "bluetooth"
+      "btusb"
+      # block loading ath_pci
+      "ath_pci"
+
+      # block loading cdrom
+      "cdrom"
+      "sr_mod"
+
+      # Framebuffer drivers are generally buggy and poorly-supported, and cause
+      # suspend failures, kernel panics and general mayhem.  For this reason we
+      # never load them automatically.
+      "aty128fb"
+      "atyfb"
+      "radeonfb"
+      "cirrusfb"
+      "cyber2000fb"
+      "cyblafb"
+      "gx1fb"
+      "hgafb"
+      "i810fb"
+      "intelfb"
+      "kyrofb"
+      "lxfb"
+      "matroxfb_base"
+      "neofb"
+      "nvidiafb"
+      "pm2fb"
+      "rivafb"
+      "s1d13xxxfb"
+      "savagefb"
+      "sisfb"
+      "sstfb"
+      "tdfxfb"
+      "tridentfb"
+      "vesafb"
+      "vfb"
+      "viafb"
+      "vt8623fb"
+      "udlfb"
+    ];
+  };
+
+}
