c7f273c3ec58a7a2ac471e949258620ca7fb166a
Daniel Minks <danielminks1230@gmail.com>
2025-12-12 11:01:46 -0500

    Fixed pkgs.system warning

--- modules/nixos/security/age.nix
+++ modules/nixos/security/age.nix
@@ -1,8 +1,8 @@
-{pkgs, lib, config, inputs, globals, system,...}:{
+{pkgs, lib, stdenv, config, inputs, globals, system,...}:{
 
   config = {
     environment.systemPackages = [
-      inputs.agenix.packages."${pkgs.system}".default
+      inputs.agenix.packages."${pkgs.stdenv.hostPlatform.system}".default
     ];
     services.openssh = {
       enable = true;
