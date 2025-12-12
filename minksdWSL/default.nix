ac9501a23d82d06c3eb181809979a14c08639ead
Daniel Minks <danielminks1230@gmail.com>
2025-12-12 11:44:37 -0500

    Swapped specialArgs for _module.args in minksdWSL

--- minksdWSL/default.nix
+++ minksdWSL/default.nix
@@ -3,39 +3,44 @@
   globals,
   overlays,
   imports,
+  system,
   ...
 }:
 
 inputs.nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
+  inherit system;
 
-    specialArgs = {
-      inherit inputs globals;
-      upkgs = import inputs.nixpkgs {inherit overlays; system = "x86_64-linux";};
-    };
+  specialArgs = {
+  };
 
-    modules = let system = "x86_64-linux"; in imports ++ [
-     inputs.home-manager.nixosModules.home-manager
-     ../modules/common
-     ../modules/nixos
-      inputs.nixos-wsl.nixosModules.default
-      {
-        wsl = {
-          enable = true;
-          defaultUser = "minksd";
-          docker-desktop.enable = true;
-        };
-	      nixpkgs.overlays = overlays;
-        system.stateVersion = "25.05";
-	      nix.settings.experimental-features = "flakes nix-command";
-	
-        networking.hostName = "minksdWSL";
-        
-        git.enable = true;
-        zsh.enable = true;
-        emacs.enable = true;
-        nh.enable = true;
-      }
-      ];
-        
+  modules = let system = "x86_64-linux"; in imports ++ [
+    inputs.home-manager.nixosModules.home-manager
+    ../modules/common
+    ../modules/nixos
+    ({config, ...}: {
+      config._module.args = {
+        inherit inputs globals;
+        upkgs = import inputs.nixpkgs {inherit overlays; system = "x86_64-linux";};
+      };
+    })
+    inputs.nixos-wsl.nixosModules.default
+    {
+      wsl = {
+        enable = true;
+        defaultUser = "minksd";
+        docker-desktop.enable = true;
+      };
+	    nixpkgs.overlays = overlays;
+      system.stateVersion = "25.05";
+	    nix.settings.experimental-features = "flakes nix-command";
+	    
+      networking.hostName = "minksdWSL";
+      
+      git.enable = true;
+      zsh.enable = true;
+      emacs.enable = true;
+      nh.enable = true;
+    }
+  ];
+  
 }
