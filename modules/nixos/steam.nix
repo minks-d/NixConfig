a9a9b46cef030af6cefd9aefd80b3805716eb886
Daniel Minks <danielminks1230@gmail.com>
2026-06-29 17:05:51 -0400

    Added stuff for steam

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -8,6 +8,7 @@
   options.steam.enable = lib.mkEnableOption "Steam game launcher.";
 
   config = lib.mkIf (config.steam.enable && pkgs.stdenv.isLinux) {
+    environment.systemPackages = [ pkgs.mangohud ];
     hardware.steam-hardware.enable = true;
     unfreePackages = [
       "steam"
@@ -19,6 +20,7 @@
       gamescopeSession.enable = true;
     };
     programs.gamescope.capSysNice = false;
+    programs.gamemode.enable = true;
 
   };
 }
