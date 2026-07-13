2a98b0dfe0fda82ccaf38a22f4336a3e476b5b7d
Daniel Minks <danielminks1230@gmail.com>
2026-07-13 13:36:38 -0400

    Removing pulse config, forgot this is a pw system

--- minksdHome/services/default.nix
+++ minksdHome/services/default.nix
@@ -2,6 +2,5 @@
   imports = [
     ./rnsd.nix
     ./rrcd.nix
-    ./pulse.nix
   ];
 }
--- minksdHome/services/pulse.nix
+++
@@ -1,8 +0,0 @@
-{...}:
-{
-  services.pulseaudio.daemon.config = {
-    default-sample-rate = 48000;
-    alternate-sample-rate = 44100;
-    default-fragments = 2;
-  };
-}
